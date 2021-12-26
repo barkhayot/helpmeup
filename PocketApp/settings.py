25baa23e70dcc924cce8a473636dd871b8af7705
Barkhayot Juraev <coderviewer@gmail.com>
2021-12-26 21:40:22 +0900

    files

--- PocketApp/settings.py
+++ PocketApp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7!@(3()3@1!&9paap1gl&&%edod7q1_mty8s(ry&q=5kvunpp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
