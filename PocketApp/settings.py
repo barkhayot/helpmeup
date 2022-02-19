fce1abc8320f66c32e8ad94d62d7f9ee682ea0dc
Barkhayot Juraev <coderviewer@gmail.com>
2022-02-19 23:23:36 +0900

    cors

--- PocketApp/settings.py
+++ PocketApp/settings.py
@@ -171,4 +171,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
 
